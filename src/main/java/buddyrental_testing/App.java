e9ff0388b84916e77d9c8af1255bcd7abdcb7d14
rsosisnsusjs <rutchaputch47@gmail.com>
2025-02-12 12:25:36 +0700

    first commit

---
+++ src/main/java/buddyrental_testing/App.java
@@ -0,0 +1,11 @@
+package buddyrental_testing;
+
+/**
+ * Hello world!
+ * Just another default file jaaaa
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
